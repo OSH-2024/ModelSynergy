fef17de33c69cbd89e9c9b8854cf0a3cfe5cf615
Qsqsdac <962114354@qq.com>
2024-06-29 16:58:56 +0800

    测试报告

--- lab4/lib/python3.9/site-packages/ray/air/data_batch_type.py
+++
@@ -1,7 +0,0 @@
-from typing import TYPE_CHECKING, Dict, Union
-
-if TYPE_CHECKING:
-    import numpy
-    import pandas
-
-DataBatchType = Union["numpy.ndarray", "pandas.DataFrame", Dict[str, "numpy.ndarray"]]
--- lab4/lib/python3.9/site-packages/ray/rllib/connectors/module_to_env/module_to_env_pipeline.py
+++
@@ -1,7 +0,0 @@
-from ray.rllib.connectors.connector_pipeline_v2 import ConnectorPipelineV2
-from ray.util.annotations import PublicAPI
-
-
-@PublicAPI(stability="alpha")
-class ModuleToEnvPipeline(ConnectorPipelineV2):
-    pass
