3189123c2eb61013cc780e56c61931cceb1ede2b
Qsqsdac <962114354@qq.com>
2024-07-03 17:06:06 +0800

    load

--- src/model_synergy/control.py
+++ src/model_synergy/control.py
@@ -3,6 +3,7 @@ import sys
 import os
 import mimetypes
 from .set import set_xattr
+from .get import get_xattr
 
 def store_command(file_path, kv_cache_path):
 
@@ -15,14 +16,25 @@ def store_command(file_path, kv_cache_path):
     ]
 
     # 设置扩展属性
-    set_xattr(file_path, "kv_cache_path", kv_cache_path.encode())
-    print("hadioas")
+    set_xattr(file_path, "user.kvcache", kv_cache_path.encode())
+
     # 调用命令
     result = subprocess.run(command, capture_output=True, text=True)
 
     # 打印命令的输出
     # print("STDOUT:", result.stdout)
     # print("STDERR:", result.stderr)
+
+def load_command(file_path):
+    has_attr, kvcache_path = get_xattr(file_path, 'user.kvcache')
+    if not has_attr:
+        print("Error: The file does not have the attribute 'user.kvcache'.")
+        return
+    command = f"python -m inf_llm.chat --model-path Qwen/Qwen1.5-0.5B-Chat --inf-llm-config-path config/qwen0b5-inf-llm.yaml --load-kv-cache-file {kvcache_path}"
+
+    os.system(command)
+    print("chat finished")
+
 
 def process_directory(directory, kv_cache_path):
     for entry in os.scandir(directory):
@@ -52,3 +64,18 @@ def main():
                 print(f"Error: The file '{file_path}' is not a text file.")
         elif os.path.isdir(file_path):
             process_directory(file_path, kv_cache_path)
+    elif sys.argv[1] == "--load" or sys.argv[1] == "-l":
+        if len(sys.argv) != 3:
+            print("Usage: ModelSynergy --load <file-path>")
+            sys.exit(1)
+        
+        file_path = sys.argv[2]
+
+        if os.path.isfile(file_path):
+            mime_type, _ = mimetypes.guess_type(file_path)
+            if mime_type and mime_type.startswith('text/'):
+                load_command(file_path)
+            else:
+                print(f"Error: The file '{file_path}' is not a text file.")
+        elif os.path.isdir(file_path):
+            print(f"Error: The file '{file_path}' is a directory.")
