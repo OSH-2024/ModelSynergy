13debf7273dd02ef1272aaeea974422a7cf8d993
00ffcc <yanqiwanrenhe@qq.com>
2024-07-04 16:47:19 +0800

    modified:   .gitignore 	modified:   init.sh 	deleted:    src/auto_modify/trans 	deleted:    src/auto_modify/watch

--- init.sh
+++ init.sh
@@ -7,7 +7,9 @@ chmod +x src/auto_modify/kill_watch.sh
 chmod +x src/task_queue/msg.sh
 
 # 执行msg.sh
-sudo src/task_queue/msg.sh
+cd src/task_queue
+sudo ./msg.sh
+cd ../..
 
 # 将 src/auto_modify/kill_watch.sh 移动至 /usr/local/bin/
 sudo cp src/auto_modify/kill_watch.sh /usr/local/bin/
