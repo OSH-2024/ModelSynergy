18f69b92e043605d700717605abddb4432e60ca1
Qsqsdac <962114354@qq.com>
2024-07-04 19:59:19 +0800

    debugging

--- src/auto_modify/trans.c
+++ src/auto_modify/trans.c
@@ -14,8 +14,10 @@ int main() {
         re_msg(file_path);
 
         //读取扩展属性
-        if(get_xattr(file_path, "user.kvcache", kv_cache_path, 4096) == -1)
+        if(get_xattr(file_path, "user.kvcache", kv_cache_path, 4096) == -1) {
+            printf("%s does not have kvcache\n", file_path);
             continue;
+        }
 
         // 使用sprintf构建命令字符串
         sprintf(command, "python -m inf_llm.gen --model-path " \
