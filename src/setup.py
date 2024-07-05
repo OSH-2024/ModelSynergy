36f7bda470b6ebf6ad260621f046b5b3059455e1
xyang <xyang@LAPTOP-G4N8F2T4.>
2024-07-05 18:14:59 +0800

    报错信息

--- src/setup.py
+++ src/setup.py
@@ -12,7 +12,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='modelsynergy',
-    version='1.0',
+    version='1.1',
     packages=find_packages(),
     install_requires=[
         'xattr==1.1.0',
